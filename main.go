1fc11bd5264dd097d7250ff8e05a3c0d662d701b
Hakan <hakankosanoglu@hotmail.com>
2020-10-18 12:15:16 +0300

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	fmt.Println("Go Docker Tutorial")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(w, "Hello World!")
+		fmt.Fprintf(w, "Hello World!")
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
